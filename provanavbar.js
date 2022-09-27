8d668d9bf25a9baac942aea9d0ebd6f94c3d3ca2
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-27 16:06:52 +0200

    u7

--- provanavbar.js
+++ provanavbar.js
@@ -1,27 +1,7 @@
-var button = document.getElementsByClassName("button")[0]
-console.log("piegnion")
+const hamburger = document.getElementsByClassName('hamburger')[0]
 
-if(button){button.addEventListener("click", hamburger)}
-var t = true
-function hamburger(){
-  var navbar = document.getElementById("navbar")
-  var nav_links = document.getElementById("nav-links")
-  if (t){
-    navbar.style.height="25px"
-    nav_links.style.visibility="hidden"
-    t = false
-  }
-  else{
-    navbar.style.height="60px"
-    nav_links.style.visibility="visible"
-    t = true
-  }
-}
 
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#nav-links").fadeToggle("slow");
-  });
+hamburger.addEventListener("click", function(){
+  console.log("in function")
+  this.classList.toggle("is-active")
 });
-
-
