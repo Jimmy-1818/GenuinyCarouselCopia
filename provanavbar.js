ab969932742865e8213e944ebfbcf810674a5d58
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-29 12:31:03 +0200

    kj

--- provanavbar.js
+++ provanavbar.js
@@ -1,7 +1,9 @@
-const hamburger = document.getElementsByClassName('hamburger')[0]
-
+const hamburger = document.getElementsByClassName("hamburger")[0]
+const menu_mobile = document.getElementsByClassName("mobile-nav")[0]
+const desktop_nav = document.getElementsByClassName("desktop-nav")[0]
 
 hamburger.addEventListener("click", function(){
-  console.log("in function")
   this.classList.toggle("is-active")
+  desktop_nav.classList.toggle("border-animation")
+  menu_mobile.classList.toggle("is-active-mobile")
 });
