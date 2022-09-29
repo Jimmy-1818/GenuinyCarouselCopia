948c348ec97e091ad6b42a566b1a5c2b16cd3225
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-29 20:33:57 +0200

    w

--- script/navbar.js
+++ script/navbar.js
@@ -20,7 +20,6 @@ window.addEventListener("scroll", () => {
     desktop_nav.classList.add("navbar-hide")
   }else{
     desktop_nav.classList.remove("navbar-hide")
-    console.log(desktop_nav)
   }
   lastSctollY = window.scrollY
 });
