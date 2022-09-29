810e0e37cbcf339e0e7a275ead05b6907fd2470d
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-09-29 14:14:25 +0200

    u

--- provanavbar.js
+++ provanavbar.js
@@ -1,9 +1,10 @@
 const hamburger = document.getElementsByClassName("hamburger")[0]
 const menu_mobile = document.getElementsByClassName("mobile-nav")[0]
 const desktop_nav = document.getElementsByClassName("desktop-nav")[0]
+const opacity = document.getElementsByClassName("opacity")[0]
 
 hamburger.addEventListener("click", function(){
   this.classList.toggle("is-active")
-  desktop_nav.classList.toggle("border-animation")
   menu_mobile.classList.toggle("is-active-mobile")
+  opacity.classList.toggle("is-active-opacity")
 });
