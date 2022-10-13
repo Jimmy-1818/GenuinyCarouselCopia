019fdfda98c77da876c7601fad735308b1a15ef5
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-10-13 15:24:39 +0200

    t

--- script/link.js
+++ script/link.js
@@ -26,21 +26,26 @@ if (document.getElementsByClassName("subscribe-steps").length > 0){
 
 
 // LINK PRODUCTION *AGGIUNGI LA CORREZIONI PER CHI INSERISCE IL LINK CON IL MESSAGGIO
-button = document.getElementById("process-link")
-if (button){button.addEventListener("click", generate_link)
-  var link_input = document.getElementById("link-to-process")
-  //link_input.addEventListener("")
+if (document.getElementById("process-link")){
+  document.getElementById("process-link").addEventListener("click", generate_link)
+  
+var link_input = document.getElementById("link-to-process")
+//link_input.addEventListener("")
 };
 
 function generate_link(){
   var affiliation_link = String(document.getElementById("link-to-process").value)
-  if(affiliation_link.length > 10){
-    var variable_affiliation = affiliation_link.slice(29)
+  if(affiliation_link.includes("https://business.genuiny.com/")){
+    var link_index = affiliation_link.indexOf("https://business.genuiny.com/")
+    var variable_affiliation = affiliation_link.slice(link_index + 29)
     var affiliate_site = "https://jimmy-1818.github.io/GenuinyCarouselCopia/index.html" + "#" + variable_affiliation
 
     document.getElementById("processed-link").value = affiliate_site
   }
-  else{
+  else if (affiliation_link.length > 10){
+    alert('Link non valido. Incolla il referral link ,che ottieni nell\'app da "condividi app".')
+  }
+  else {
     alert('Incolla prima il referral link, che ottieni nell\'app da "condividi app".')
   }
 };
