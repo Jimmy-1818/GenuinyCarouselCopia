9c67f37e410612eca7da91a9111e9e69e398cd8f
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-10-14 22:30:14 +0200

    a

--- script/home_functionalities.js
+++ script/home_functionalities.js
@@ -11,3 +11,4 @@ function onFullScreen() {
 document.getElementsByClassName("milena-spiega")[0].addEventListener('webkitfullscreenchange', onFullScreen)
 document.getElementsByClassName("milena-spiega")[0].addEventListener('mozfullscreenchange', onFullScreen)
 document.getElementsByClassName("milena-spiega")[0].addEventListener('fullscreenchange', onFullScreen)
+document.getElementsByClassName("milena-spiega")[0].addEventListener('msfullscreenchange', onFullScreen)
