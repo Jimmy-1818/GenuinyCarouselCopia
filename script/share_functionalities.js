1b0c9d8bc416b69f85367ca141b6ca3e83a855b0
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-10-15 10:40:41 +0200

    d

--- script/share_functionalities.js
+++ script/share_functionalities.js
@@ -1,24 +1,12 @@
-console.log("__")
+const input = document.getElementById("processed-link")
+const copy_btn = document.getElementById("copy-link")
 
+copy_btn.onclick = function(){
+  if (input.value !== null){
 
-if (document.getElementById("copy-link")){
-  document.getElementById("copy-link").addEventListener("click", copy_link)
-}
-//function copy_link(){
-//  navigator.clipboard.writeText(document.getElementById("processed-link").value)
-//  alert("Copiato negli appunti")
-//};
-
-
-<button id="btn1" data-clipboard-text="Text to copy goes here">
-    Copy to clipboard
-</button>
-<button id="btn2">Click here to change data-clipboard-text</button>
-
-<br /><br />
-
-<input type="text" placeholder="Paste here to see clipboard" />
-
-new ClipboardJS("#btn1");
-document.querySelector("#copy-link").addEventListener("click", () => 
-  document.querySelector("#processed-link").dataset.clipboardText = Math.random());
+  }
+  // Select text
+  input.select();
+  // Copy the text
+  document.execCommand("Copy");
+};
