10a8ffeba2c4d49282e109a722192a172b9df35b
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-10-15 11:23:51 +0200

    j

--- script/share_functionalities.js
+++ script/share_functionalities.js
@@ -3,10 +3,13 @@ const copy_btn = document.getElementById("copy-link")
 
 copy_btn.onclick = function(){
   if (input.value !== null){
-
-  }
   // Select text
   input.select();
   // Copy the text
   document.execCommand("Copy");
+  alert("Link Copiato!")
+  }
+  else{
+    alert("Impossibile copiare, inserisci prima il link.")
+  }
 };
