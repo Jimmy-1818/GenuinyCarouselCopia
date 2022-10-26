0c8423ac8c696e760178521dfa0545300045cab3
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-10-26 18:01:26 +0200

    jh

--- script/subscribe_functionalities.js
+++ script/subscribe_functionalities.js
@@ -6,6 +6,7 @@ generate_btn.addEventListener("click",()=>{
     let link_desktop = document.getElementById("link_desktop")
     if(!qr_link) return;
     qr_image.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qr_link}&size=[780]x[780]`;
+    // The API might have problems once over 10,000 requests a day (--> warn the API or change method)
     link_desktop.href = qr_link
     link_desktop.innerHTML = qr_link
    
