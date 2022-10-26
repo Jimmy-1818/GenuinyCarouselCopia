327404ef775bb1606d6ed7f562f3c8d352d1efbf
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-10-26 17:24:32 +0200

    d

--- script/subscribe_functionalities.js
+++ script/subscribe_functionalities.js
@@ -1,15 +1,12 @@
-function generate_qr(){
-	console.log("oj")
-}
-
 const generate_btn = document.getElementById("generate_qr_btn")
 const qr_image = document.getElementById("qr_image")
 
-
-/* This block of code executes when the user clicks on the generate button after entering a value(i.e a valid url or even a text) */
 generate_btn.addEventListener("click",()=>{
     let qr_link = document.getElementById("link").href
+    let link_desktop = document.getElementById("link_desktop")
     if(!qr_link) return;
-    qr_image.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qr_link}`;
+    qr_image.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qr_link}&size=[780]x[780]`;
+    link_desktop.href = qr_link
+    link_desktop.innerHTML = qr_link
    
 });
