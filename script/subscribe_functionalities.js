81b38a5893f43bfd7f59ff2da02b4a40070b95d4
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-10-28 15:39:52 +0200

    f

--- script/subscribe_functionalities.js
+++ script/subscribe_functionalities.js
@@ -9,6 +9,7 @@ generate_btn.addEventListener("click",()=>{
     var qr_link = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qr_destination}&size=[780]x[780]`;
     if (qr_image.src != qr_link){
         qr_image.src = qr_link
+        qr_image.src = qr_link
     } 
     // The API might have problems once over 10,000 requests a day (--> warn the API or change method)
     link_desktop.href = qr_destination
