3b6a9127c78aff011725943ec6de4e40b70591cc
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-10-28 15:35:47 +0200

    t

--- script/subscribe_functionalities.js
+++ script/subscribe_functionalities.js
@@ -3,12 +3,15 @@ const qr_image = document.getElementById("qr_image")
 
 
 generate_btn.addEventListener("click",()=>{
-    let qr_link = document.getElementById("link").href
+    let qr_destination = document.getElementById("link").href
     let link_desktop = document.getElementById("link_desktop")
-    if(!qr_link) return;
-    qr_image.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qr_link}&size=[780]x[780]`;
+    if(!qr_destination){return};
+    var qr_link = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qr_destination}&size=[780]x[780]`;
+    if (qr_image.src != qr_link){
+        qr_image.src = qr_link
+    } 
     // The API might have problems once over 10,000 requests a day (--> warn the API or change method)
-    link_desktop.href = qr_link
-    link_desktop.innerHTML = qr_link
+    link_desktop.href = qr_destination
+    link_desktop.innerHTML = qr_destination
    
 });
