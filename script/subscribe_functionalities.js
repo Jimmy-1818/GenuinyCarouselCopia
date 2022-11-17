cbd3c40d5a66955b28d9a86558fb3900599833bf
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-11-17 14:19:33 +0100

    5

--- script/subscribe_functionalities.js
+++ script/subscribe_functionalities.js
@@ -24,3 +24,14 @@ generate_btn.addEventListener("click",()=>{
     link_desktop.innerHTML = qr_destination
    
 });
+
+
+var qrcode = undefined;
+function generateQRCode(value){
+    if (qrcode === undefined){
+        qrcode = new QRCode(document.getElementById("qrcode"), value);
+    }else{
+        qrcode.clear()
+        qrcode.makeCode(value);
+    }
+}
